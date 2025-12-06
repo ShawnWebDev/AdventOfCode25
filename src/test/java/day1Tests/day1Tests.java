610d3dffa3947ecfcbc380051d2bf6fa9e701969
shawn <shawnby222@gmail.com>
2025-12-06 15:16:14 -0600

    adds day1

--- src/test/java/day1Tests/day1Tests.java
+++ src/test/java/day1Tests/day1Tests.java
@@ -4,15 +4,19 @@ import com.webdev.day1.Day1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-// mostly for testing runtime speeds
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
 public class day1Tests {
 
+    // mostly for testing runtime speeds
     @Test
     @DisplayName("Test day 1 runtime")
     public void testDay1() {
         Day1 day1 = new Day1();
         int answer = day1.runDay1();
+        assertEquals(5657,answer);
     }
 
 }
