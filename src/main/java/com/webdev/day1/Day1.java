022dce59c46831d2a8bbe4d1927fe0adcdf8d743
shawn <shawnby222@gmail.com>
2025-12-06 15:02:23 -0600

    adds day1

---
+++ src/main/java/com/webdev/day1/Day1.java
@@ -0,0 +1,44 @@
+package com.webdev.day1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import java.util.Scanner;
+
+public class Day1 {
+    static void main() {
+        Day1 day1 = new Day1();
+        day1.runDay1();
+    }
+
+    public int runDay1() {
+        int zeroCount = 0;
+        File input = new File("src/main/resources/day1_Input.txt");
+        int dialPos = 50;
+
+        try {
+            Scanner sc = new Scanner(input);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                int direction = String.valueOf(line.charAt(0)).equals("L") ? -1 : 1;
+                int amount = Integer.parseInt(line.substring(1));
+
+                for (int i = 0; i < amount; i++) {
+                    dialPos = (dialPos + direction) % 100;
+
+                    if (dialPos == 0) {
+                        zeroCount++;
+                    }
+                }
+            }
+            sc.close();
+
+        } catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(zeroCount);
+        return zeroCount;
+    }
+}
---
+++ src/test/java/day1Tests/day1Tests.java
@@ -0,0 +1,18 @@
+package day1Tests;
+
+import com.webdev.day1.Day1;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+// mostly for testing runtime speeds
+
+public class day1Tests {
+
+    @Test
+    @DisplayName("Test day 1 runtime")
+    public void testDay1() {
+        Day1 day1 = new Day1();
+        int answer = day1.runDay1();
+    }
+
+}
